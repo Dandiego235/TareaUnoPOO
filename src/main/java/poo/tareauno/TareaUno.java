2ef951a22871d34d9399f8c625b150487247fc47
Daniel Granados <dagranados@estudiantec.cr>
2022-08-22 18:34:28 -0600

    Avance 2: Agregar avión

--- src/main/java/poo/tareauno/TareaUno.java
+++ src/main/java/poo/tareauno/TareaUno.java
@@ -6,6 +6,7 @@
 package poo.tareauno;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  *
@@ -13,7 +14,45 @@ import java.util.Scanner;
  */
 public class TareaUno {
 
-    static void agregarAvion(){
+    static void agregarAvion(String[][][] paviones, String[] pavionesId){
+        System.out.println("Agregar avión"); 
+        Scanner leerEntrada = new Scanner(System.in);
+        String id; // String para la identificación del avión
+        do{
+            System.out.println("Ingrese el identificador del avión (String de 5 caracteres exactos)");
+            id = leerEntrada.nextLine(); // Lee la entrada
+            if (id.length() != 5){ //  Si la entrada no es exactamente de 5 caracteres, envía error y la vuelve a pedir.
+                System.out.println("ERROR: El string ingresado debe ser de exactamente 5 caracteres.");
+            }
+            // FALTA REVISAR SI EL AVION YA ESTA REGISTRADO.
+        } while (id.length() != 5);
+        
+        int avion = 0; // Se inicializa el índice del avión vacío que se va a agregar.
+        for (; avion < pavionesId.length; avion++){ // Se recorre el arreglo de los IDs de los aviones para encontrar el primero que esté vacío.
+            if("".equals(pavionesId[avion])){
+                pavionesId[avion] = id; // Se asigna la identificación y se sale.
+                break;
+            }
+        }
+        
+        String zeroPad; // Variables para construir el identificador de cada asiento.
+        char clase;
+        char letra;
+        for(int fila = 0; fila < paviones[avion].length; fila++){
+            for(int asiento = 0; asiento < paviones[avion][fila].length; asiento++){ // Se recorren las filas y asientos.
+                zeroPad = (fila + 1 < 10) ? "0" : ""; // Si la fila es del 1 al 9, se rellena con 0 adelante.
+                clase = (fila < 4) ? 'J' : 'E'; // Si la fila es de las primeras 4, la clase es J, si no es E.
+                letra = (char) asiento; // Se inicializa la letra como el asiento para luego convertirla a su caracter.
+                if (fila < 4 && asiento > 1){
+                    letra += 67; // Si la fila es de las primeras 4 y el asiento es a partir del 2, se suma 67
+                    // para convertirlos en E y F.
+                } else {
+                    letra += 65; // A los demás se le suma 65 para convertirlos en A, B, C, D, E y F.
+                }
+                paviones[avion][fila][asiento] = zeroPad + (fila + 1) + letra + clase + "A";
+                // Se construye el identificador.
+            }
+        }
     }
     
     static void modificarCapacidadAsientos(){
@@ -44,9 +83,10 @@ public class TareaUno {
     }
     
     static void consultarAsientosDisponibles(){
-        
+
     }
     
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
         final short NUM_AVIONES = 20;
         final short FILAS = 25;
@@ -55,15 +95,18 @@ public class TareaUno {
         for (int avion = 0; avion < aviones.length; avion++){ // se recorre la matriz para asignarle la longitud correcta a cada fila.
             for (int fila = 0; fila < aviones[avion].length; fila++){
                 if (fila < 4){ // si el índice de la fila es menor que 4, la fila tiene solo 4 asientos
-                    aviones[avion][fila] = new String[4];
+                    aviones[avion][fila] = new String[] {"", "", "", ""};
                 } else { // si es mayor o igual que 4, tiene 6 asientos.
-                    aviones[avion][fila] = new String[6];
+                    aviones[avion][fila] = new String[] {"", "", "", "", "", ""};
                 }
             }
         }
         
-        String[] avionesId = new String[NUM_AVIONES]; // Matriz donde se almacenan los identificadores de los aviones.
+        String[] avionesId = new String[NUM_AVIONES]; // Matriz donde se almacenan los identificadores de los aviones.        
         // Tiene una relación 1 a 1 con los índices de la primera dimensión de la matriz aviones.
+        for (int index = 0; index < avionesId.length; index++){ // se llena el arreglo de strings vacios
+            avionesId[index] = "";
+        }
         
         Scanner leerDato = new Scanner(System.in);
         
@@ -85,7 +128,7 @@ public class TareaUno {
             opcion = leerDato.nextInt();
             switch (opcion){
                 case 1:
-                    agregarAvion();
+                    agregarAvion(aviones, avionesId);
                     break;
                 case 2:
                     modificarCapacidadAsientos();
